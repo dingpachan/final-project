71c85606ecf485e340c799868139cda74d33ee92
dingpachan <dingpachan01@gmail.com>
2015-01-16 17:29:44 +0800

    test4

--- js/login.js
+++ js/login.js
@@ -28,32 +28,34 @@ $(document).ready(function(){
 	
 	$(document).ready(function(){
 		$(document).on("submit", "#signUp", function(e) {
-    	e.preventDefault();
+    		e.preventDefault();
 		Parse.initialize("c4azxhlcBAObQPSxQ6jY4lf8dbFEPBD6y7zgIHD0", "2uGIxUaShVOXpBWyn4UwRoPK1Tali8MrKf4mp0Xp");
-    		var re = /^[A-Za-z0-9]*$/;
-		var username = $("#new_username").val(); 
-		var password = $("#new_password").val();
-		var id_ok = re.exec(username);
-		var pwd_ok = re.exec(password);
-		if(id_ok && pwd_ok){
-			var user = new Parse.User();
-			user.set("username", username);
-			user.set("password", password);
-			user.signUp(null, {  
-			success: function(user) {
-				// Do stuff after successful login.
-				alert('恭喜你，註冊成功!');
-				window.location.href = "index.html"; 
-				},
-				error: function(user, error) {
-				// The login failed. Check error to see why.
-				alert('此帳號已被使用過了喔!');
-				//alert(username);
-				//alert(password);
+			var username = $("#new_username").val(); 
+			var password = $("#new_password").val();
+			if(username && password !=null){
+				var re = /^[A-Za-z0-9]+$/;
+				var id_ok = re.exec(username);
+				var pwd_ok = re.exec(password);
+				if(id_ok && pwd_ok){
+					var user = new Parse.User();
+					user.set("username", username);
+					user.set("password", password);
+					user.signUp(null, {  
+						success: function(user) {
+						// Do stuff after successful login.
+						alert('恭喜你，註冊成功!');
+						window.location.href = "index.html"; 
+						},
+						error: function(user, error) {
+							// The login failed. Check error to see why.
+							alert('此帳號已被使用過了喔!');
+						}
+					});	
+				}else{
+					alert("帳號密碼格式錯誤");
 				}
-			});	
-		}else{
-			alert("帳號密碼格式錯誤");	
-		}
+			}else{
+				alert("請輸入帳號密碼!");
+			}
 		});		
 	});
