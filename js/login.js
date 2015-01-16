1d058bd78aa23fe3c10f0eb05e33772791eaabd9
dingpachan <dingpachan01@gmail.com>
2015-01-16 17:08:37 +0800

    test3

--- js/login.js
+++ js/login.js
@@ -30,28 +30,30 @@ $(document).ready(function(){
 		$(document).on("submit", "#signUp", function(e) {
     	e.preventDefault();
 		Parse.initialize("c4azxhlcBAObQPSxQ6jY4lf8dbFEPBD6y7zgIHD0", "2uGIxUaShVOXpBWyn4UwRoPK1Tali8MrKf4mp0Xp");
-		//var username="sandy";
-    	//var password="12345";
+    		var re = /\w/;
 		var username = $("#new_username").val(); 
 		var password = $("#new_password").val();
-		//alert("signUp");
-		//alert(username);
-		//alert(password);
-		var user = new Parse.User();
-		user.set("username", username);
-		user.set("password", password);
-		user.signUp(null, {  
+		var id_ok = re.exec(username);
+		var pwd_ok = re.exec(password);
+		if(id_ok && pwd_ok){
+			var user = new Parse.User();
+			user.set("username", username);
+			user.set("password", password);
+			user.signUp(null, {  
 			success: function(user) {
-			// Do stuff after successful login.
-			alert('恭喜你，註冊成功!');
-			window.location.href = "index.html"; 
-			},
-			error: function(user, error) {
-			// The login failed. Check error to see why.
-			alert('此帳號已被使用過了喔!');
-			///alert(username);
-			//alert(password);
-			}
-		});
+				// Do stuff after successful login.
+				alert('恭喜你，註冊成功!');
+				window.location.href = "index.html"; 
+				},
+				error: function(user, error) {
+				// The login failed. Check error to see why.
+				alert('此帳號已被使用過了喔!');
+				//alert(username);
+				//alert(password);
+				}
+			});	
+		}else{
+			alert("帳號密碼格式錯誤");	
+		}
 		});		
 	});
